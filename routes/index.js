1589deb44160c433ea2d8e50d5e031a87e1de4d1
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-02 14:20:41 -0500

    Display Pokepunk with image, quote, and container ID

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const getRandomPokepunk = require('../utils/random-pokepunk');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pokepunk' });
+  res.render('index', { pokepunk: getRandomPokepunk() });
 });
 
 module.exports = router;
